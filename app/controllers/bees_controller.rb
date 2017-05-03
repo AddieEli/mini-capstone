ea90dd16a58f060ea6772fb1014b18acbb093364
AddieEli <acadavid71@gmail.com>
2017-05-02 21:30:18 -0500

    adding info to mini

--- app/controllers/bees_controller.rb
+++ app/controllers/bees_controller.rb
@@ -1,2 +1,9 @@
 class BeesController < ApplicationController
+ 
+  def all_bees
+   @bee = Bee.all
+  
+   render 'bee.html.erb' 
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/bee' => 'bees#all_bees'
 end
