f920f939f362c448c060393a41fcf14e83bbb305
AddieEli <acadavid71@gmail.com>
2017-05-02 21:14:50 -0500

    added in bee instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+bee = Bee.new(
+       name: "Honey Queen Bee",
+       price: 20,
+       image: "https://www.wired.com/wp-content/uploads/2015/08/GettyImages-71666555.jpg",
+       description: "She lays eggs."
+              )
+bee.save 
+
+bee = Bee.new(
+                name: "Honey Worker Bee",
+                price: 5,
+                image: "http://bee-pollen-health.com/Worker-Bees-Top.jpg",
+                description: "These ladies are guards, nurses, and forage"
+              )
+bee.save
+
+bee = Bee.new(
+                name: "Honey Drone Bee",
+                price: 1,
+                image: "http://typesofbees.info/wp-content/uploads/2014/07/types-of-bee-drone-bee.jpg",
+                description: "They are supposed to mate with the queen and die at the end of summer. BOY BYE"
+
+              )
+bee.save
+
+bee = Bee.new(
+                name: "African Bee",
+                price: 60,
+                image: "http://www.goldenoak.org/safety-training-library/africanized-honey-bees/",
+                description: "Very aggressive breed of honey bee, RUN."
+)
+bee.save
+
+bee = Bee.new(
+                name: "Bubble Bee",
+                price: 15,
+                image: "https://a-z-animals.com/media/animals/images/original/bumble_bee3.jpg",
+                description: "This bee lives alone and in the ground, it will live for one season."
+)
+bee.save
+
