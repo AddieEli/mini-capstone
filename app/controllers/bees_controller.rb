5ac1865db77e7a1f0eaad7c7433e81f55e9025d4
AddieEli <acadavid71@gmail.com>
2017-05-07 16:57:39 -0500

    added forms

--- app/controllers/bees_controller.rb
+++ app/controllers/bees_controller.rb
@@ -5,5 +5,25 @@ class BeesController < ApplicationController
   
    render 'bee.html.erb' 
   end 
+
+  def index
+    @bees = Bee.all
+  end 
   
+  def show
+    bee_id = params[:id]
+    @bee = Bee.find_by(id: bee_id)
+  end 
+
+  def new
+  end
+  
+  def create
+      bee = Bee.new(
+                    name: params[:name],
+                    price: params[:price],
+                    description: params[:description]
+                    )
+      bee.save
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get '/bee' => 'bees#all_bees'
+ 
+  get '/bees' => 'bees#index'
+  get '/' => 'bees#index'
+
+  get '/bees/new' => 'bees#new'
+  post '/bees' => 'bees#create'
+
+  get '/bees/:id' => 'bees#show'
 end
